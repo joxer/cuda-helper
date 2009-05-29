7265a257c1872fe173a65555ba93896bd2aa8ec6
joxer <kell.92.k@gmail.com>
2009-05-29 22:35:55 +0200

    modified:   ../lib/cuda-helper.c 	modified:   a.out 	modified:   allocate.cu

--- lib/cuda-helper.c
+++ lib/cuda-helper.c
@@ -24,10 +24,10 @@ struct Matrix* GetMatrixFromDevice(struct Matrix* d_m){
 
 struct Matrix* SetMatrixOnDevice(struct Matrix* h_m){
   
-  struct Matrix* d_m = MatrixAllocateOnDevice(h_m->x, h_m->y);
-  cudaMemcpy2D(h_m->matrix, d_m->x * sizeof(int), d_m->matrix, d_m->pitch, d_m->x * sizeof(int) , d_m->y,cudaMemcpyDeviceToHost);
-
-
+  struct Matrix* d_m = MatrixAllocateOnDevice(h_m->x,h_m->y);
+  cudaMemcpy2D(d_m->matrix, h_m->pitch, h_m->matrix, h_m->x*sizeof(int), h_m->x * sizeof(int) , h_m->y,cudaMemcpyDeviceToHost);
+  return h_m;
+  
 }
 
 int Matrix_mul(Matrix* m_a, Matrix* m_b, Matrix* m_c){  
--- test/allocate.cu
+++ test/allocate.cu
@@ -3,10 +3,13 @@
 
 int main(){
 
-  struct  Matrix *m_d = MatrixAllocateOnDevice(4, 4);
+  struct Matrix* m_d = MatrixAllocateOnDevice(4, 4);
   struct Matrix* m_h = GetMatrixFromDevice(m_d); 
-  
-  printf(" %d %d %d ", m_d->pitch, m_d->x, m_d->y);
+  struct Matrix* m_b = SetMatrixOnDevice(m_h);
+
+  printf(" %d %d %d \n", m_b->pitch, m_b->x, m_b->y);
   printf(" %d %d %d ", m_h->pitch, m_h->x, m_h->y);
+  
+
   return 0;
 }

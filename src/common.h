ef065f0e9a407e97f7ec9caae4bc2194dbbe2417
joxer <kell.92.k@gmail.com>
2009-05-30 14:44:18 +0200

    deleted:    lib/cuda-allocation.c 	modified:   src/common.h 	modified:   src/cuda-allocation.h

--- src/common.h
+++ src/common.h
@@ -1,13 +1,5 @@
 #include "cuda-allocation.h"
 
 
-struct Matrix {
-  int *matrix;
-  int x;
-  int y;
-  size_t pitch;
-  
-};
-
 
 
--- src/cuda-allocation.h
+++ src/cuda-allocation.h
@@ -1,3 +1,14 @@
-struct Matrix MatrixAllocateOnDevice(int width, int height, int size);
-struct Matrix* GetMatrixFromDevice(struct Matrix* m);
+
+struct Matrix {
+  int *matrix;
+  int x;
+  int y;
+  size_t pitch;
+  
+};
+
+
+struct Matrix* MatrixAllocateOnDevice(int width, int height, int size);
+struct Matrix* MatrixAllocateOnHost(int width, int height);
+struct Matrix* GetMatrixFromDevice(struct Matrix* d_m);
 struct Matrix* SetMatrixOnDevice(struct Matrix* h_m);

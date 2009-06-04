485d9a24758053f091e0a31e2aa83d2ad19717b4
joxer <joxer@anacreon.(none)>
2009-06-04 23:49:00 +0200

    modified:   src/cuda-structs.h 	deleted:    src/struct.c

--- src/cuda-structs.h
+++ src/cuda-structs.h
@@ -3,13 +3,12 @@ struct Matrix {
   int x;
   int y;
   size_t pitch;
-  
+  unsigned char position; // 0 = host, 1 = device
 };
 
 struct Vector {
 
-
   int* vector;
   int width;
-
+  unsigned char position; // 0 = host, 1 = device
 };
--- src/struct.c
+++
@@ -1,7 +0,0 @@
-struct Matrix {
-  int *matrix;
-  int x;
-  int y;
-  size_t pitch;
-  
-};

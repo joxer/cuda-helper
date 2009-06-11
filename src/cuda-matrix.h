9da7a28b7ef4467cdb5cf307159861be9817658b
joxer <joxer@anacreon.(none)>
2009-06-11 22:34:12 +0200

    add matrix

---
+++ src/cuda-matrix.h
@@ -0,0 +1,4 @@
+struct Matrix* MatrixSum(struct Matrix* first, struct Matrix* second);
+struct Matrix* MatrixSub(struct Matrix* first, struct Matrix* second);
+struct Matrix* MatrixMul(struct Matrix* first, struct Matrix* second);
+struct Matrix* MatrixDiv(struct Matrix* first, struct Matrix* second);
